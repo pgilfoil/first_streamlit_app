164ebd4d86db82345298ac7034cf5c08fcb6a99c
Patrick Gilfoil <111374781+pgilfoil@users.noreply.github.com>
2022-08-16 11:43:28 -0400

    Update streamlit_app.py
    fix bug in streamlit reference

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlist.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
