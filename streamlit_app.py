737858160a0a7a357f34496e9af49b90ad43991c
Patrick Gilfoil <111374781+pgilfoil@users.noreply.github.com>
2022-08-23 06:30:00 -0400

    updated formatting of fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,9 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+# normalizes the json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# outputs normalized text to a table
+streamlit.dataframe(fruityvice_normalized)
