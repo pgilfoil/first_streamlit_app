ef0cac95c2aa0c09fa206d4353945c78d4263a7c
Patrick Gilfoil <111374781+pgilfoil@users.noreply.github.com>
2022-08-30 10:08:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
   
 def get_fruit_load_list(connection):
-  with my_cnx.cursor() as my_cur:
+  with connection.cursor() as my_cur:
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
   
