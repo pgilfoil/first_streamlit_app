8f4595a20c7cbb01a67697fdc6e192f860dc28ba
Patrick Gilfoil <111374781+pgilfoil@users.noreply.github.com>
2022-08-30 08:56:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -31,3 +32,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs normalized text to a table
 streamlit.dataframe(fruityvice_normalized)
+
